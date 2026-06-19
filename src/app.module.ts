a80bdd83d8a8fcf0776d8abf5206fe808ecdbd41
Tantelyy <tantelynyainaa@gmail.com>
2026-06-19 10:30:48 +0300

    feat: integrate Prisma ORM with PostgreSQL and set up initial database schema
    - Added Prisma configuration and service for database connection.
    - Created initial migration for Users, Roles, Permissions, UserPermissions, and RolePermissions tables.
    - Defined Prisma schema models corresponding to the database tables.
    - Implemented entities for User, Role, Permission, UserPermission, and RolePermission.
    - Updated app module to include Prisma and feature modules for users, roles, and permissions.
    - Added dotenv support for environment variable management.
    - Enhanced TypeScript configuration for better strictness.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PermissionsModule } from './permissions/permissions.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UsersModule,
+    RolesModule,
+    PermissionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/permissions/entities/permission.entity.ts
@@ -0,0 +1,11 @@
+export class PermissionEntity {
+  id: number;
+  label: string;
+  code: string;
+  descriptions: string | null;
+  createdBy: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+  deletedBy: number | null;
+}
